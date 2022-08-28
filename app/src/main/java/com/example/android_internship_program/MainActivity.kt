bdb05319a59a92dcd9cdfd97b612d0b3059a9b2a
qenawi <aldmanmero@yahoo.com>
2022-08-28 14:02:24 +0200

    init commit

--- app/src/main/java/com/example/android_internship_program/MainActivity.kt
+++ app/src/main/java/com/example/android_internship_program/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.android_internship_program
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+
+  /*
+    MainActivity: notes
+   */
+
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
+
+
 }
